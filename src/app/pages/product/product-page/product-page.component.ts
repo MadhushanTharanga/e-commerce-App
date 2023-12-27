c37ac4fba490ddbb611f7cdb2394423e5d677471
MadhushanTharanga <madhuwa07@gmail.com>
2023-12-27 20:22:18 +0530

    added some pages and routes home and product page

--- src/app/pages/product/product-page/product-page.component.ts
+++ src/app/pages/product/product-page/product-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ProductViewCardsComponent} from "../../../share/cards/product-view-cards/product-view-cards.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductViewCardsComponent,
+    RouterLink
+  ],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css'
 })
